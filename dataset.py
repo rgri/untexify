eae0beff3dfa4530d9911211851dae2e88a1d7eb
rgri <robert13grimley@gmail.com>
2023-03-25 17:09:57 -0400

    Finished setting up model

--- dataset.py
+++ dataset.py
@@ -42,3 +42,27 @@ class_names = train_ds.class_names
 #         plt.axis("off")
 # plt.show()
 num_classes = len(class_names)
+
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Rescaling(1.0 / 255, input_shape=(img_height, img_width, 3)),
+        tf.keras.layers.Conv2D(16, 3, padding="same", activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, padding="same", activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(64, 3, padding="same", activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(num_classes),
+    ]
+)
+
+model.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+epochs = 10
+# history = model.fit(trains_ds)

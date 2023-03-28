f80e33cf150b8e704677f4d4043b60b9557a0691
rgri <robert13grimley@gmail.com>
2023-03-28 09:52:50 -0400

    Added bounding box to draw region

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-// matches elements of the document "document" (presumably the default instance of the Document() object instantiated by call "defer" in the script element) which have "class=myCanvas".[fn:5]
 const canvas = document.querySelector('.myCanvas');
-const backgroundCanvas = document.querySelector('.background')
-const width = canvas.width = 72;
-const height = canvas.height = 72;
-const background = backgroundCanvas.getContext('2d')
+const width = canvas.width = 72
+const height = canvas.height = 72
 const ctx = canvas.getContext('2d');
 
-const backgroundWidth = 77;
-const backgroundHeight = 77;
-// background.fillStyle = 'rgb(0,0,0)';
-// background.fillRect(0, 0, width, height);
-ctx.fillStyle = 'rgb(0,0,0)';
-ctx.strokeStyle = 'blue';
-ctx.rect(0, 0, backgroundWidth, backgroundHeight);
-
+ctx.fillStyle = 'rgb(255,255,255)';
 ctx.fillRect(0, 0, width, height);
 
-const colorPicker = document.querySelector('input[type="color"]');
-const sizePicker = 4;
+const colorPicker = 'rgb(0,0,0)';
+const sizePicker = 2;
 const output = document.querySelector('.output');
 const clearBtn = document.querySelector('button');
 
@@ -28,7 +18,6 @@ function degToRad(degrees) {
 
 // update sizepicker output value
 
-// sizePicker.addEventListener('input', () => output.textContent = sizePicker.value);
 
 // store mouse pointer coordinates, and whether the button is pressed
 let curX;
@@ -44,15 +33,15 @@ document.addEventListener('mousemove', e => {
 canvas.addEventListener('mousedown', () => pressed = true);
 
 canvas.addEventListener('mouseup', () => pressed = false);
-
-clearBtn.addEventListener('click', () => {
-  ctx.fillStyle = 'rgb(0,0,0)';
-  ctx.fillRect(0, 0, width, height);
-});
+canvas.
+  clearBtn.addEventListener('click', () => {
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(0, 0, width, height);
+  });
 
 function draw() {
   if (pressed) {
-    ctx.fillStyle = colorPicker.value;
+    ctx.fillStyle = colorPicker;
     ctx.beginPath();
     ctx.arc(curX, curY - 85, sizePicker, degToRad(0), degToRad(360), false);
     ctx.fill();

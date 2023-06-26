405a868a54c1af73c932d60a71ff28623558fd93
rgri <robert13grimley@gmail.com>
2023-06-25 23:35:04 -0500

    Messing around with stuff

--- default.nix
+++ default.nix
@@ -1,3 +1,3 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> { } p2n ? (builtins.fetchFromGitHub {};)}:
 let untexifyEnv = pkgs.poetry2nix.mkPoetryEnv { projectDir = ./.; };
 in untexifyEnv.env
--- flake.nix
+++ flake.nix
@@ -3,16 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
-    poetry2nix.url = "github:nix-community/poetry2nix";
+    poetry2nix.url =
+      "github:nix-community/poetry2nix/4f91d45e39cf64b642c6d2725c7bd50d6fcb544b";
     utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, poetry2nix, utils, ... }@inp:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        overlays = [ poetry2nix.overlay ];
       in {
         devShell = pkgs.mkShell {
-          buildInputs = [ (poetry2nix.mkPoetryEnv { projectDir = ./.; }) ];
+          buildInputs = [
+            (pkgs.poetry2nix.mkPoetryEnv {
+              projectDir = ./.;
+              overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+                # …
+                # workaround https://github.com/nix-community/poetry2nix/issues/568
+                structlog = super.structlog.overridePythonAttrs (old: {
+                  buildInputs = (old.buildInputs or [ ])
+                    ++ [ pkgs.python310.flit-core ];
+                });
+              });
+            })
+          ];
+
         };
       });
 

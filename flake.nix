5e85ca863d56b0ac63f02ed845c7a268450c26a5
rgri <robert13grimley@gmail.com>
2023-06-25 20:40:58 -0500

    Testing flakes with poetry2nix

--- flake.nix
+++ flake.nix
@@ -2,34 +2,20 @@
   description = "Flake to manage python workspace";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
-    flake-utils.url = "github:numtide/flake-utils";
-    mach-nix.url = "github:DavHau/mach-nix?ref=3.3.0";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, mach-nix }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, poetry2nix, nixpkgs, utils, ... }@inp:
+    utils.lib.eachDefaultSystem (system:
       let
-        python = "python310"; # <--- change here
-        pkgs = nixpkgs.legacyPackages.${system};
-        # https://github.com/DavHau/mach-nix/issues/153#issuecomment-717690154
-        mach-nix-wrapper = import mach-nix { inherit pkgs python; };
-        requirements =
-          builtins.readFile ./frontend/untexifyweb/requirements.txt;
-        pythonBuild = mach-nix-wrapper.mkPython { inherit requirements; };
+        pkgs = import nixpkgs { inherit system; };
+        p2n = import poetry2nix { inherit system; };
       in {
-        inherit pythonBuild;
         devShell = pkgs.mkShell {
-          buildInputs = [
-            # dev packages
-            (pkgs.${python}.withPackages
-              (ps: with ps; [ pip black pyflakes isort ])) # <--- change here
-            pkgs.nodePackages.pyright
-            pkgs.glpk
-
-            # app packages
-            pythonBuild
-          ];
+          buildInputs = [ (p2n.mkPoetryEnv { projectDir = ./.; }) ];
         };
       });
+
 }

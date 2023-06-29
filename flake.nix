31a02d1ba9aa983c8500f2cfbe4ef3df61725115
rgri <robert13grimley@gmail.com>
2023-06-29 16:34:29 -0500

    Initial commit

--- flake.nix
+++ flake.nix
@@ -3,19 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    poetry2nix.url = "github:nix-community/poetry2nix";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, poetry2nix, nixpkgs, utils, ... }@inp:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        p2n = import poetry2nix { inherit system; };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [ (p2n.mkPoetryEnv { projectDir = ./.; }) ];
-        };
-      });
+  outputs = { self, nixpkgs, utils, ... }@inp:
+    utils.lib.eachDefaultSystem
+    (system: let pkgs = import nixpkgs { inherit system; }; in rec { });
 
 }

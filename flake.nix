6125c3699cc8406f885bd0d1014f263407ff2f8f
rgri <robert13grimley@gmail.com>
2023-07-15 08:17:11 -0500

    Development flake
    This flake includes a working `nix develop` that pulls in poetry. Ideal
    for inter-computer operation. Not necessarily perfect- need automated
    environment/direnv shit.

--- flake.nix
+++ flake.nix
@@ -5,30 +5,13 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/";
   inputs.poetry2nix = { url = "github:nix-community/poetry2nix"; };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
         inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        packages = {
-          myapp = mkPoetryApplication {
-            projectDir = self;
-            overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend
-              (self: super: {
-                lazy-loader = super.lazy-loader.overridePythonAttrs (old: {
-                  buildInputs = (old.buildInputs or [ ]) ++ [ super.flit-core ];
-                });
-                beniget = super.beniget.overridePythonAttrs (old: {
-                  buildInputs = (old.buildInputs or [ ])
-                    ++ [ super.setuptools-scm ];
-                });
-              });
-          };
-          default = self.packages.${system}.myapp;
-        };
-
         devShells.default =
           pkgs.mkShell { packages = [ poetry2nix.packages.${system}.poetry ]; };
       });

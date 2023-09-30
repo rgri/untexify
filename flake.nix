6ccb39e0506c00cf520d68564c8d13572b3d3264
rgri <robert13grimley@gmail.com>
2023-09-30 16:48:57 -0500

    Fixed ld_library_path (i think?)
    There were a few errors:
    [[id:a279f2b7-efca-4529-8c6a-af8a05c491d0]]
    [[id:33aa9083-0b83-42a9-8966-28f8a4058491]]
    Related to the project which I've resolved by exposing gcc as a
    dependency. It's namespaced from a flake input so hopefully it is truly
    reproducible now.

--- flake.nix
+++ flake.nix
@@ -3,32 +3,30 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/";
+  inputs.nixpkgsOld.url =
+    "github:Nixos/nixpkgs/6e3a86f2f73a466656a401302d3ece26fba401d9";
   inputs.poetry2nix = { url = "github:nix-community/poetry2nix"; };
   inputs.poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }@inputs:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, nixpkgsOld }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
         inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
         pkgs = nixpkgs.legacyPackages.${system};
+        pkgsOld = nixpkgsOld.legacyPackages.${system};
         system = "x86_64-linux";
-        pythonPkgs = (import (builtins.fetchTarball {
-          sha256 = "1dvqwqki44v6s6adlmdy0lw3lm0z53ml6cd6i6wymni2ns1wpzy1";
-          url =
-            "https://github.com/NixOS/nixpkgs/archive/6e3a86f2f73a466656a401302d3ece26fba401d9.tar.gz";
-        })) { inherit system; };
       in {
         devShells.default = pkgs.mkShell {
-          LD_LIBRARY_PATH = "${pythonPkgs.stdenv.cc.cc.lib}/lib";
+          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [
+            pkgsOld.gcc-unwrapped.lib
+            pkgs.gcc-unwrapped.lib
+          ];
           packages = [
-            pythonPkgs.python3Full
+            pkgsOld.python3Full
             pkgs.poetry
             pkgs.pyenv
-            pkgs.nodePackages.npm
-            pkgs.nodePackages.rollup
             #FIXME: You don't need two installs of pyright.
-            pkgs.nodePackages.pyright
             pkgs.nodePackages.mermaid-cli
           ];
         };

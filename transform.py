69716988531c5177c8aecfa489cb20de48f8d271
rgri <robert13grimley@gmail.com>
2023-10-09 23:17:55 -0500

    Comment updates

--- transform.py
+++ transform.py
@@ -9,18 +9,22 @@ import numpy as np
 # Next, we Sharpen() the image twice to severely denoise.
 # Equalize() will remove color differences to match the input method on the frontend
 # Sigma is "squiggliness", and Alpha is movement? alpha_affine is how much it moves across the page.
+# p is the probability a transform will be applied
 transform = A.Compose(
     [
-        A.Sharpen(alpha=(1, 1), lightness=(1.0, 1.0), p=1.0),
-        A.ElasticTransform(alpha=20, sigma=10000, alpha_affine=10, p=1),
-        A.Sharpen(alpha=(1, 1), lightness=(1.0, 1.0), p=1.0),
-        A.Sharpen(alpha=(1, 1), lightness=(1.0, 1.0), p=1.0),
-        A.Equalize(p=1.0),
+        A.Defocus(p=1, radius=100),
+        # A.Downscale(scale_min=0.07, scale_max=0.07, p=1),
+        # A.Sharpen(alpha=(1, 1), lightness=(1.0, 1.0), p=1.0),
+        # A.ElasticTransform(alpha=20, sigma=20000, alpha_affine=10, p=1),
+        # A.Downscale(scale_min=0.07, scale_max=0.07, p=1),
+        # A.Sharpen(alpha=(1, 1), lightness=(1.0, 1.0), p=1.0),
+        # A.Sharpen(alpha=(1, 1), lightness=(1.0, 1.0), p=1.0),
+        # A.Equalize(p=1.0),
     ]
 )
 
 # Get the first command line argument- sys.argv[0] is always the command itself
-image = cv2.imread("inputimage.png" )
+image = cv2.imread("inputimage.png")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 transformed = transform(image=image)["image"]

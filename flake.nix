c435ddb6312b550d61365cdd882ebe3c37c8126f
rgri <robert13grimley@gmail.com>
2023-10-11 20:01:32 -0500

    Revert "Updated flake to use poetry2nix"
    This reverts commit fd82c03ad90d2ddc174ec15b764eea7811555d5f.

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/";
   inputs.nixpkgsOld.url =
     "github:Nixos/nixpkgs/6e3a86f2f73a466656a401302d3ece26fba401d9";
+  inputs.poetry2nix = { url = "github:nix-community/poetry2nix"; };
+  inputs.poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix, nixpkgsOld }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
@@ -15,49 +17,22 @@
         pkgsOld = nixpkgsOld.legacyPackages.${system};
         system = "x86_64-linux";
       in {
-        devShells.default = let
-
-          appEnv = pkgs.poetry2nix.mkPoetryEnv {
-            python = pkgs.python3Full;
-            overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend
-              (self: super: {
-                lazy-loader = super.lazy-loader.overridePythonAttrs (old: {
-                  buildInputs = (old.buildInputs or [ ]) ++ [ super.flit-core ];
-                });
-
-                werkzeug = super.werkzeug.overridePythonAttrs (old: {
-                  buildInputs = (old.buildInputs or [ ]) ++ [ super.flit-core ];
-                });
-                django-bootstrap5 = super.django-bootstrap5.overridePythonAttrs
-                  (old: {
-                    buildInputs = (old.buildInputs or [ ])
-                      ++ [ super.hatchling ];
-                  });
-                tensorflow-io-gcs-filesystem =
-                  super.tensorflow-io-gcs-filesystem.overridePythonAttrs (old: {
-                    buildInputs = (old.buildInputs or [ ])
-                      ++ [ super.tensorflow-bin ];
-                  });
-                ml-dtypes = super.ml-dtypes.overridePythonAttrs (old: {
-                  buildInputs = (old.buildInputs or [ ])
-                    ++ [ super.setuptools super.pybind11 ];
-                });
-                contourpy = super.contourpy.overridePythonAttrs (old: {
-                  nativeBuildInputs = (old.nativeBuildInputs or [ ])
-                    ++ [ super.meson-python ];
-                });
-              });
-            projectDir = ./.;
-          };
-
-        in appEnv.env.overrideAttrs (oldAttrs: {
-          buildInputs = [
+        devShells.default = pkgs.mkShell {
+          SECRET_KEY = "dummy";
+          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [
+            pkgsOld.gcc-unwrapped.lib
+            pkgsOld.libglvnd
+            pkgsOld.glib
+            # Only necessary for transform.py
+            pkgsOld.zlib
+          ];
+          packages = [
+            pkgsOld.python3Full
+            pkgs.poetry
             pkgs.pyenv
             #FIXME: You don't need two installs of pyright.
             pkgsOld.nodePackages.pyright
           ];
-
-          SECRET_KEY = "dummy";
-        });
+        };
       });
 }

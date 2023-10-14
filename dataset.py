4a35be388712264a26e3f7e22d9fdcbfbf98d099
Robbi <robert13grimley@gmail.com>
2023-10-14 09:16:54 -0500

    Updated dataset and transform (edit this)
    Split into more coherent parts.

--- dataset.py
+++ dataset.py
@@ -16,16 +16,22 @@ from keras.models import Sequential
 datadir = "/home/shortcut/git/untexify-data/"
 original_images = os.listdir(datadir + "original_images")
 
-# The transforms compenents were chosen as follows:
-# Sharpen() will preliminarily de-noise the image.
-# ElasticTransform() "squiggles" the image to simulate handwriting.
-# Next, we Sharpen() the image twice to severely denoise.
-# Equalize() will remove color differences to match the input method on the frontend
-transform = A.Compose(
+rotateTransform = A.Compose(
     [
-        A.Downscale(scale_min=0.07, scale_max=0.07, p=1),
+        A.CropAndPad(
+            p=1,
+            px=(15, 90),
+            pad_cval=1000,
+            pad_mode=cv2.BORDER_CONSTANT,
+        ),
+    ]
+)
+# See [[id:c77b0738-55b0-4e40-acfd-d941e8794e5d]]
+wiggleTransform = A.Compose(
+    [
+        A.Downscale(scale_min=0.10, scale_max=0.10, p=1),
         A.ElasticTransform(alpha=20, sigma=20000, alpha_affine=10, p=1),
-        A.Downscale(scale_min=0.07, scale_max=0.07, p=1),
+        A.Downscale(scale_min=0.10, scale_max=0.10, p=1),
         A.Equalize(p=1.0),
     ]
 )
@@ -56,11 +62,24 @@ def helper(index):
         print()
     # Sigma is "squiggliness", and Alpha is movement? alpha_affine is how much it moves across the page.
     # DONE: Clean up this comment.
-    transformed = transform(image=image)["image"]
-    transformed = np.array(transformed)
+    rotated = rotateTransform(image=image)["image"]
+    bw_rotated = cv2.cvtColor(rotated, cv2.COLOR_RGB2GRAY)
+    threshold_rotated = bw_rotated[:]
+    threshold = 1
+
+    h, b = bw_rotated.shape[:2]
+    for i in range(h):
+        for j in range(b):
+            if bw_rotated[i][j] > threshold:
+                threshold_rotated[i][j] = 255
+            else:
+                threshold_rotated[i][j] = 0
+
+    wiggle_transformed = wiggleTransform(image=threshold_rotated)["image"]
+
     outName = datadir + "images/" + i[:-4] + "/" + str(j) + ".png"
 
-    cv2.imwrite(outName, transformed)
+    cv2.imwrite(outName, wiggle_transformed)
 
 
 # Generate the class directories
--- transform.py
+++ transform.py
@@ -15,7 +15,7 @@ rotateTransform = A.Compose(
     [
         A.CropAndPad(
             p=1,
-            px=60,
+            px=(15, 90),
             pad_cval=1000,
             pad_mode=cv2.BORDER_CONSTANT,
         ),
@@ -24,14 +24,13 @@ rotateTransform = A.Compose(
 # See [[id:c77b0738-55b0-4e40-acfd-d941e8794e5d]]
 wiggleTransform = A.Compose(
     [
-        A.Downscale(scale_min=0.07, scale_max=0.07, p=1),
+        A.Downscale(scale_min=0.10, scale_max=0.10, p=1),
         A.ElasticTransform(alpha=20, sigma=20000, alpha_affine=10, p=1),
-        A.Downscale(scale_min=0.07, scale_max=0.07, p=1),
+        A.Downscale(scale_min=0.10, scale_max=0.10, p=1),
         A.Equalize(p=1.0),
     ]
 )
 
-# Get the first command line argument- sys.argv[0] is always the command itself
 image = cv2.imread("inputimage.png")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 

6ef22845a98fd0371eaac57303e6608d913e1b54
rgri <robert13grimley@gmail.com>
2023-10-18 03:10:32 -0500

    Routing and removing extraneous pages
    Makes it so just navigating to the webpage displays the bootstrap.html
    page immediately.

--- frontend/untexifyweb/testapp/urls.py
+++ frontend/untexifyweb/testapp/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:x>/", views.quadratic, name="quadratic"),
-    path("home/", views.get_drawing, name="home"),
+    path("", views.get_drawing_bootstrap, name="bootstrap"),
     path("bootstrap/", views.get_drawing_bootstrap, name="bootstrap"),
 ]

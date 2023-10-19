399889060e9c600293c88bf8003a940eec2d432a
rgri <robert13grimley@gmail.com>
2023-10-19 16:28:53 -0500

    i am very smart
    MORE black magic:
    1. setting LD_LIBRARY_PATH blows up a lot of nixpkgs (like my js lsp)
    
    2. tensorflow is really only complaining about the missing standard c
    library file
    
    3. LD_PRELOAD is a var specifically designed to add specific .so files
    to the environment
    
    4. LD_PRELOAD is not LD_LIBRARY_PATH (doesn't mess with it I think)
    
    so i just hard-coded (relative to a pinned stdenv) the path to
    libstdc++.so.6 into LD_PRELOAD and delete LD_LIBRARY_PATH...
    
    wah lah: js lsp (and presumably other nixpkgs stuff) works, tensorflow works

--- flake.nix
+++ flake.nix
@@ -19,14 +19,14 @@
       in {
         devShells.default = pkgs.mkShell {
           SECRET_KEY = "dummy";
-          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [
+          LD_PRELOAD = nixpkgs.lib.makeLibraryPath [
             # pkgsOld.gcc-unwrapped.lib
             # pkgsOld.libglvnd
             # pkgsOld.glib
             # # Only necessary for transform.py
             # pkgsOld.zlib
             pkgsOld.stdenv.cc.cc
-          ];
+          ] + "/libstdc++.so.6";
           packages = [
             pkgsOld.python3Full
             pkgs.poetry
